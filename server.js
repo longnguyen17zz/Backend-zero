90b9365bd592bc404e2264fa5006840a5419aefa
longnguyen17zz <longhq17@gmail.com>
2024-05-15 10:03:44 +0700

    video 13

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World\nNguyễn Văn Long');
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
