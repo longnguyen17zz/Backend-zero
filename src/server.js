1e1c3d0f5408f68784f6b30aeb0a35b5066c96c4
longnguyen17zz <longhq17@gmail.com>
2024-07-13 04:29:51 +0700

    b21

--- src/server.js
+++ src/server.js
@@ -2,17 +2,21 @@ const express = require('express') // commonjs
 const path = require('path')
 require('dotenv').config()
 // check env 
-console.log(process.env)
+// console.log(process.env)
 
 const app = express() // app express 
-const port = process.env  // port 
+const port = process.env.PORT || 8888  // port 
+const hostname = process.env.HOST_NAME
 
 // config template engine 
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 
+// config static files
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World with Long and nodemon!')
 })
 
 app.get('/longhq', (req, res) => {
