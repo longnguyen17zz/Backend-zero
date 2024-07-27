4be60aa3cfbdaa003502eda8a88fa209e25e397e
longnguyen17zz <longhq17@gmail.com>
2024-07-27 10:30:10 +0700

    B39

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,19 +1,8 @@
-const connection = require('../config/database')
-
-const getHomePage = (req, res)=> {
-    // let users = [];
-
-    // connection.query(
-    //     'Select * from Users u',
-    //     function (err, results, fields) {
-    //         users = results;
-    //         console.log(">>>results: ",results); // results contains rows returned by server
-    //         // console.log(">>>fileds: ",fields); // fields contains extra meta data about results, if available
-    //         console.log(">>> check users: ", users)
-    //         res.send(JSON.stringify(users));
-    //     }
-    //   );
-    return res.render('home.ejs');
+const connection = require('../config/database');
+const {getAllUsers} = require('../services/CRUDservices');
+const getHomePage = async (req, res)=> {
+    let results = await getAllUsers();
+    return res.render('home.ejs', {listUsers: results});
 }
 
 const getlonghq = (req, res)=> {
